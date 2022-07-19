4aa5a48d8d7fb7f440302fe38d4c5365739d985a
Derek Roberts <derek.roberts@gmail.com>
2022-07-19 15:19:57 -0700

    Stop using --schema=public and run as postgres user

--- fom-prod-data-move.sh
+++ fom-prod-data-move.sh
@@ -13,12 +13,11 @@ NEW_NS="a4b31c-tools"
 
 # Vars
 LEADER=$(oc get cm/fom-db-ha-prod-leader -o json | jq -j '.metadata.annotations.leader')
-OLD_USER=$(oc exec -n ${OLD_NS} $LEADER -- printenv | grep APP_USER | sed 's/.*=//g')
 OLD_DB=$(oc exec -n ${OLD_NS} $LEADER -- printenv | grep APP_DATABASE | sed 's/.*=//g')
 
 # Dump and copy down
 oc exec -n ${OLD_NS} $LEADER -- mkdir -p /tmp/dump
-oc exec -n ${OLD_NS} $LEADER -- pg_dump -U $OLD_USER -d $OLD_DB -Fc -f /tmp/dump/dump_old --no-privileges --no-tablespaces --schema=public --no-owner
+oc exec -n ${OLD_NS} $LEADER -- pg_dump -U postgres -d $OLD_DB -Fc -f /tmp/dump/dump_old --no-privileges --no-tablespaces --no-owner
 oc cp -n ${OLD_NS} $LEADER:/tmp/dump .
 
 
@@ -38,15 +37,16 @@ oc exec -n ${NEW_NS} $POD -- pg_restore -d $NEW_DB /tmp/dump_old -U $NEW_USER -c
 
 # Tables
 echo "--- old ---"
-oc exec -n ${OLD_NS} $LEADER -- psql -U $OLD_USER -d $OLD_DB -c "select count (*) from pg_tables;"
+oc exec -n ${OLD_NS} $LEADER -- psql -U postgres -d $OLD_DB -c "select count (*) from pg_tables;"
 echo "--- new ---"
 oc exec -n ${NEW_NS} $POD -- psql -U $NEW_USER -d $NEW_DB -c "select count (*) from pg_tables;"
 
 # Create new db_dump and compare to old one
-oc exec -n ${NEW_NS} $POD -- pg_dump -d $NEW_DB -U $NEW_USER -Fc -f /tmp/dump_new --no-privileges --no-tablespaces --schema=public --no-owner
+oc exec -n ${NEW_NS} $POD -- pg_dump -d $NEW_DB -U $NEW_USER -Fc -f /tmp/dump_new --no-privileges --no-tablespaces --no-owner
 oc exec -n ${NEW_NS} $POD -- ls -l /tmp/
 
 ### Cleanup
+
 oc exec -n ${OLD_NS} $LEADER -- rm -rf /tmp/dump
 oc exec -n ${NEW_NS} $POD -- rm /tmp/dump_new /tmp/dump_old
 rm dump_old
--- fom-test-data-move.sh
+++ fom-test-data-move.sh
@@ -10,12 +10,11 @@ TARGET=${1:-test}
 
 # Vars
 LEADER=$(oc get cm/fom-db-ha-test-leader -o json | jq -j '.metadata.annotations.leader')
-OLD_USER=$(oc exec $LEADER -- printenv | grep APP_USER | sed 's/.*=//g' )
 OLD_DB=$(oc exec $LEADER -- printenv | grep APP_DATABASE | sed 's/.*=//g')
 
 # Dump and copy down
 oc exec $LEADER -- mkdir -p /tmp/dump
-oc exec $LEADER -- pg_dump -U $OLD_USER -d $OLD_DB -Fc -f /tmp/dump/dump_old --no-privileges --no-tablespaces --schema=public --no-owner
+oc exec $LEADER -- pg_dump -U postgres -d $OLD_DB -Fc -f /tmp/dump/dump_old --no-privileges --no-tablespaces --no-owner
 oc cp $LEADER:/tmp/dump .
 
 
@@ -35,16 +34,17 @@ oc exec $POD -- pg_restore -d $NEW_DB /tmp/dump_old -U $NEW_USER -c --no-owner |
 
 # Tables
 echo "--- old ---"
-oc exec $LEADER -- psql -U $OLD_USER -d $OLD_DB -c "select count (*) from pg_tables;"
+oc exec $LEADER -- psql -U postgres -d $OLD_DB -c "select count (*) from pg_tables;"
 echo "--- new ---"
 oc exec $POD -- psql -U $NEW_USER -d $NEW_DB -c "select count (*) from pg_tables;"
 
 # Create new dump, compare to old one and clean up
-oc exec $POD -- pg_dump -d $NEW_DB -U $NEW_USER -Fc -f /tmp/dump_new --no-privileges --no-tablespaces --schema=public --no-owner
+oc exec $POD -- pg_dump -d $NEW_DB -U $NEW_USER -Fc -f /tmp/dump_new --no-privileges --no-tablespaces --no-owner
 oc exec $POD -- ls -l /tmp/
 
 
 ### Cleanup
+
 oc exec $LEADER -- rm -rf /tmp/dump
 oc exec $POD -- rm /tmp/dump_new /tmp/dump_old
 rm dump_old
